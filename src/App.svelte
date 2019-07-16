70a6f38fa38df7fbce6805092bf0be0baa7f5ed0
paiva-thiago <paiva.thiago@gmail.com>
2019-07-16 13:46:47 -0300

    Customiza Mais!

--- src/App.svelte
+++ src/App.svelte
@@ -3,29 +3,40 @@
 	import Gondola from './Gondola.svelte'
 	import Footer from './Footer.svelte'
 	let promise = pegaLojinha()
-
+	let sociaLinks = [
+						{"social":"facebook",  "url" :   ""},
+						{"social":"twitter",   "url" :   "paivocodes"},
+						{"social":"github"   , "url" :   "paiva-thiago"},
+						{"social":"pinterest", "url" :   ""},
+						{"social":"lastfm"   , "url" :   "cheimesears"},
+						{"social":"linkedin" , "url" :   "thiagormp"}
+					];
 	Array.prototype.shuffle = function(){
 	return this.map((a) => ({sort: Math.random(), value: a}))
 			.sort((a, b) => a.sort - b.sort)
 			.map((a) => a.value)
 	}
-
+	
 	async function pegaLojinha() {
-		const res = await fetch(`resources/stock.json`)
+		let ret={}
+		const res = await fetch(`resources/data.json`)
 		const text = await res.text()
 		let result = []
 		if (res.ok) {
 			result= JSON.parse(text)
 		} else {
 			throw new Error(text)
-			return result
+			return ret
 		}
-		result = await result.map((x)=>{
+		ret = result
+		ret.gondola = await ret.gondola.map((x)=>{
 			x.src='./images/'.concat(x.img).concat('.jpg')
 			x.valor= parseFloat(x.valor).toFixed(2).replace('.',','  )
   			return x
 		})
-		return result.shuffle()
+		ret.gondola = result.gondola.shuffle()	
+		
+		return ret
 	}
 
 </script>
@@ -33,6 +44,7 @@
 <style>
 @import url('https://fonts.googleapis.com/css?family=Barriecito&display=swap');
 @import url('https://fonts.googleapis.com/css?family=Raleway');
+@import url('https://weloveiconfonts.com/api/?family=zocial');
 * {
     padding: 0;
     margin: 0;
@@ -44,17 +56,20 @@
       font-family: 'Raleway', sans-serif;
 
   }   
-    
+  .sos{color:red;font-size: 18px; font-family: monospace;}
 </style>
+<svelte:head>
+	<title>ShopShop - Sua loja estática!</title>
+</svelte:head>
 
 <body>
 	{#await promise}
-		<p>...waiting</p>
-	{:then itens}
-		<Header title="Loja ©"/>
-		<Gondola itens={itens}/>
+		<p class="sos">aguarde... ou peça socorro!</p>
+	{:then retorno}
+		<Header title={retorno.tituloLoja} socials={retorno.socials} img={retorno.imgBanner} />
+		<Gondola itens={retorno.gondola}/>
 	{:catch error}
-		<p style="color: red">{error.message}</p>
+		<p class="sos">{error.message}</p>
 	{/await}
 	<Footer/>
 </body>
--- src/Header.svelte
+++ src/Header.svelte
@@ -1,18 +1,58 @@
 <script>
     export let title;
+    export let socials;
+    export let img;
 </script>
 <style>    
+   
+
+    /* zocial */
+    [class*="zocial-"]:before {
+        font-family: 'zocial', sans-serif;
+    }
+
     h1{
         font-family: 'Barriecito';
+        width:60vw;
+        float: left;
+        color: #555;
     }
     img.banner{
         margin:0 auto;
-        width:90vw;
+        width:100vw;
         height: 500px;
         object-fit: cover;
+    }
+    .socialbar{float:right;width:30vw;}
+    .socialbar a{
+        border-radius: 15%;
+        border: 1px solid #EEE;
+        background: #FFF;
+        color: #555;
+        width:75px;
+        height: 25px;
+        padding-top:12.5px;
+        padding-bottom:12.5px;
+        margin-left:10px;
+        float:right;
+        vertical-align: middle;
+        text-align: center;
+    }
+    .socialbar a:hover{
+        color: #FFF;
+        background-color:#555;
+        text-decoration:none;
     }
 </style>
 <header>
     <h1>{title}</h1>
-    <img class="banner" src="./layout-images/store-open.jpg" alt="Photo by Alexandre Godreau on Unsplash">
+      <span class="socialbar">
+        {#each socials as {zocialIcon, url}, idx}
+            {#if url!==''}
+                <a href="{url}" ><span class="zocial-{zocialIcon}"></span></a>
+            {/if}
+        {/each}
+    </span>
+    <img class="banner" src="{img.url}" alt="{img.desc}">
+  
 </header>
